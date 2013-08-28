db100fa10972afd010a41dd0b470844477c2f042
Adam <raisadamo@gmail.com>
2013-08-28 16:58:43 +0100

    Add validations to movie model

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,15 @@
 class Movie < ActiveRecord::Base
   attr_accessible :cast_member_ids, :cast_members, :director, :genres, :length, :plot, :poster, :rating, :release_date, :tagline, :trailer_url, :title
+
+  validates :cast_member_ids, presence: true
+  validates :cast_members, presence: true
+  validates :director, presence: true
+  validates :genres, presence: true
+  validates :length, presence: true
+  validates :plot, presence: true
+  validates :rating, presence: true
+  validates :release_date, presence: true
+  validates :tagline, presence: true
+  validates :title, presence: true
+
 end
