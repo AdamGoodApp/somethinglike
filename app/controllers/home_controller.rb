d33f779ffacf26bcfb30ded3ed54246f52297de3
Adam <raisadamo@gmail.com>
2013-08-28 16:57:19 +0100

    Ready for saving movies to db

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,6 +4,7 @@ class HomeController < ApplicationController
 
 	def index
 		@search = Imdb::Search.new(params[:query])
+		binding.pry
 	end
 
 
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,3 @@
 class Movie < ActiveRecord::Base
-  attr_accessible :cast_member_ids, :cast_members, :director, :genres, :length, :plot, :poster, :rating, :release_date, :tagline, :trailer_url
+  attr_accessible :cast_member_ids, :cast_members, :director, :genres, :length, :plot, :poster, :rating, :release_date, :tagline, :trailer_url, :title
 end
