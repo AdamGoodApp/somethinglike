a039f7c34c7d44b64539ddce3790addf44f1db92
Adam <raisadamo@gmail.com>
2013-08-31 12:51:13 +0100

    Add Ajex search

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,3 +13,9 @@
 //= require jquery
 //= require jquery_ujs
 //= require_tree .
+
+$(function(){
+  $("#search_form").on("keyup", function() {
+		$(this).submit();	
+  });
+});
--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -3,24 +3,33 @@ class MoviesController < ApplicationController
 
 
 	def index
-		# @movies = Movie.all
 
-  #   respond_to do |format|
-  #     format.html # index.html.erb
-  #     format.json { render json: @movies }
-  #   end
 
   	@q = Movie.search(params[:q])
+  	# puts "_________"
+  	# puts params[:q]
+   # 	puts "_________"
+
   	if params[:q]
 			@result = @q.result(:distinct => true)
 		else
 			@result = {}
 		end
 
-		a = Imdb::Top250.new.movies
-		binding.pry
-	end
 
+
+
+		# @movies = Movie.all
+
+    # respond_to do |format|
+    #   format.html # index.html.erb
+    #   format.js
+    #   format.json { render json: @result }
+    # end
+
+		# a = Imdb::Top250.new.movies
+		# binding.pry
+	end
 
 
 
