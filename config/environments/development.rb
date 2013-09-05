ca157e29a9b81df4701562f56b6a52e29c231f08
Adam <raisadamo@gmail.com>
2013-09-05 20:41:01 +0100

    Tryin config

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -8,7 +8,7 @@ SomethingLike::Application.configure do
 
   # Log error messages when you accidentally call methods on nil.
   config.whiny_nils = true
-
+config.assets.initialize_on_precompile = false
   # Show full error reports and disable caching
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
